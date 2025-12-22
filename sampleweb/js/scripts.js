71f62417ae0536bf44e7bb5d5e9efa5eafacafd3
esmameral <esma.meral@gmail.com>
2025-12-22 18:46:59 +0300

    Bootstrap added

--- sampleweb/js/scripts.js
+++ sampleweb/js/scripts.js
@@ -1,5 +1,4 @@
 function loadInstructors() {
-
     fetch('http://localhost:8081/instructors')
         .then(response => response.json())
         .then((instructors) => {
@@ -8,17 +7,41 @@ function loadInstructors() {
            instructors.forEach(element => {
             selectText += `<option value='${element.id}'> ${element.name}</option>`;
            });
-                
-             
             selectText += "</select>";
             document.getElementById("instructorDiv").innerHTML = selectText
         }
         );
+}
 
+function loadCourses() {
+    fetch('http://localhost:8081/courses')
+        .then(response => response.json())
+        .then((courses) => {
+            printCourses(courses);
+        }
+        );
+}
 
-   
-
-
+function printCourses(courses) {
+    var text = ""; 
+    courses.forEach(course => {
+        text += `
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div class="card h-100 shadow-sm">    
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">${course.courseCode}</h5>
+            <p class="card-text">${course.title}</p>
+            <p class="card-text">Credit:${course.credit}</p>
+            <p class="card-text">Quota:${course.quota}</p>
+            <h5 class="card-text">${course.instructor}</h5>
+            <a href="#" class="btn btn-primary mt-auto">Detail</a>
+          </div>
+        </div>
+      </div>
+    `;
+    });
+    var container = document.getElementById("courseList")
+    container.innerHTML = text;
 }
 
 function createCourse() {
